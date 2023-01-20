da4467dc4f33914529f0406e1ab20f8136708279
Richard Haddad <richard@haddads.net.au>
2023-01-20 23:31:39 +1000

    build: pricing page setup

--- resources/js/Pages/Pricing.jsx
+++ resources/js/Pages/Pricing.jsx
@@ -6,6 +6,15 @@ import d3ToPng from "d3-svg-to-png";
 import Standard from "../Layouts/Standard.jsx";
 
 const Pricing = () => {
+  const pricingTiers = [
+    {
+      title: "Starter",
+      cost: "Free",
+      mainFeatures: [],
+      generalFeatures: [],
+      coa: "Create Free Account",
+    },
+  ];
   return (
     <>
       <Helmet>
@@ -20,31 +29,11 @@ const Pricing = () => {
             <section className="py-12 lg:px-12 rounded">
               <h3 className="text-xl uppercase font-bold">Account</h3>
               <h2 className="-mt-2 font-header text-4.5xl">Pricing</h2>
-              <article className="mx-auto mt-12 w-full max-w-lg.5">
-                <p className="py-4 font-light text-xl leading-9">
-                  If we were to sum up what QR codes are in one sentence, it'd
-                  be this:{" "}
-                  <span className="font-normal">
-                    QR Codes are barcodes on steroids, in every way.
-                  </span>
-                </p>
-                <p className="py-4 font-light text-xl leading-9">
-                  Where barcodes are one-dimensional and capable of generating
-                  around 10 trillion unique codes, QR Codes are two-dimensional
-                  with near an infinite number of unique codes. Where barcodes
-                  are used predominantly for products and retail,{" "}
-                  <strong className="font-normal">
-                    QR Codes can be used for anything!
-                  </strong>
-                </p>
-                <p className="py-4 font-light text-xl leading-9">
-                  Sharing a website with many people? Offering WIFI access
-                  safely and seamlessly? Setting up an easier way for diners to
-                  order from the menu? Collecting feedback for a product?
-                </p>
-                <p className="py-4 font-light text-xl font-normal leading-9">
-                  QR Codes are perfect for all of that and more.
-                </p>
+              <article className="mx-auto mt-12 flex flex-row w-full max-w-lg.5">
+                <div>Starter</div>
+                <div>Plus</div>
+                <div>Pro</div>
+                <div>Enterprise</div>
               </article>
             </section>
           </div>
