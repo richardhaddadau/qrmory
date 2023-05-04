a1bd00413c249e37674b018b428ce0be7bee6094
Richard Haddad <richard@haddads.net.au>
2023-05-05 00:06:40 +1000

    build: deleteSubscriber function in MailerLite

--- resources/js/Helpers/MailerLite.js
+++ resources/js/Helpers/MailerLite.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const mailerLiteGroups = {
+  "Free User": 87228887969302501,
+};
+
 const newSubscriber = (data) => {
   let config = {
     method: "POST",
@@ -45,3 +49,29 @@ const findSubscriberByEmaiil = (email) => {
       console.log(err);
     });
 };
+
+const deleteSubscriber = (email) => {
+  let config = {
+    method: "DELETE",
+    maxBodyLength: "Infinity",
+    url:
+      "https://connect.mailerlite.com/api/subscribers/" +
+      findSubscriberByEmaiil(email),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.REACT_APP_MAILERLITE_API_KEY,
+    },
+  };
+
+  axios
+    .delete(config)
+    .then((r) => {
+      const response = r.data;
+      return response;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { mailerLiteGroups, newSubscriber, deleteSubscriber };
