89a3683d4f00eea7d00ff22756487b3f81ec4109
richardhaddadau <richard@haddads.net.au>
2023-05-04 18:23:12 +1000

    build: set up firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// create a new cloud function to add new subscribers
+exports.addSubscriberToMailerlite = functions.auth
+  .user()
+  .onCreate(async (user) => {
+    try {
+      const subscriber = {
+        email: user.email,
+        fields: {
+          name: user.displayName || "N/A",
+        },
+      };
+    } catch (error) {
+      console.log(
+        `Error adding subscriber ${user.id} to Mailerlite: ${error.message}`
+      );
+    }
+  });
