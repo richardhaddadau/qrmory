10cfaed410a0d148599081c2eb13ebaebaea5cba
Richard Haddad <richard@haddads.net.au>
2023-05-04 20:02:45 +1000

    build: early functions

--- resources/js/Helpers/MailerLite.js
+++ resources/js/Helpers/MailerLite.js
@@ -1,11 +1,47 @@
-// create a function that uses axios to make an api call to mailerlite.com
+import axios from "axios";
 
-const axios = require("axios");
+const newSubscriber = (data) => {
+  let config = {
+    method: "POST",
+    maxBodyLength: "Infinity",
+    url: "https://connect.mailerlite.com/api/subscribers",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.REACT_APP_MAILERLITE_API_KEY,
+    },
+    data: JSON.stringify(data),
+  };
 
-const mailerlite = axios.create({
-  baseURL: "https://api.mailerlite.com/v2",
-  headers: {
-    "Content-Type": "application/json",
-    "X-Api-Key": process.env.MAILERLITE_API_KEY,
-  },
-});
+  axios
+    .post(config)
+    .then((r) => {
+      const response = r.data;
+      return response;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const findSubscriberByEmaiil = (email) => {
+  let config = {
+    method: "POST",
+    maxBodyLength: "Infinity",
+    url: "https://connect.mailerlite.com/api/subscribers/" + email,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.REACT_APP_MAILERLITE_API_KEY,
+    },
+    data: JSON.stringify(data),
+  };
+
+  axios
+    .get(config)
+    .then((r) => {
+      const response = r.data.id;
+      return response;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
