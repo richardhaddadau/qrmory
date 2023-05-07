052a67d783d53daec36b4b8b55a3de68c6e4cbe4
Richard Haddad <richard@haddads.net.au>
2023-05-07 21:43:59 +1000

    build: update YouTube control to match main

--- resources/js/Components/Controls/YoutubeQR.jsx
+++ resources/js/Components/Controls/YoutubeQR.jsx
@@ -1,66 +1,29 @@
-import { useState } from "react";
-
 const YoutubeQR = ({ setText, setChanged, setNewQR }) => {
-  // States
-  const [urlType, setUrlType] = useState("video");
-  const [youTubeURL, setYouTubeURL] = useState(
-    "https://www.youtube.com/watch?v="
-  );
+    return (
+        <>
+            <label className="control-label">
+                Enter YouTube Link:
+                <div className="flex flex-col xl:flex-row flex-nowrap">
+                    <input
+                        id="youtube-input"
+                        type="text"
+                        className={
+                            "control-input"
+                        }
+                        onChange={(el) => {
+                            if (el.target.value) {
+                                setText(el.target.value);
+                            } else {
+                                setText("");
+                            }
 
-  return (
-    <>
-      <label className="control-label">
-        Enter YouTube detail:
-        <div className="flex flex-col xl:flex-row flex-nowrap">
-          <select
-            id="protocol-selector"
-            className="mr-2 mt-1 rounded-xl w-44 text-stone-500"
-            onChange={(el) => {
-              const youtubeInput =
-                document.getElementById("youtube-input").value;
-
-              if (el.target.value === "video") {
-                setYouTubeURL("https://www.youtube.com/watch?v=");
-              } else {
-                setYouTubeURL("https://www.youtube.com/");
-              }
-
-              if (youtubeInput) {
-                setText(youTubeURL + youtubeInput);
-              } else {
-                setText("");
-              }
-
-              setNewQR(false);
-              setChanged(true);
-            }}
-          >
-            <option value="video">Video</option>
-            <option value="username">Username</option>
-          </select>
-          <p className="pt-2 text-qrmory-purple-400 font-bold text-lg">
-            {youTubeURL}
-          </p>
-          <input
-            id="youtube-input"
-            type="text"
-            className={
-              "control-input " + (urlType === "video" ? "shortest" : "")
-            }
-            onChange={(el) => {
-              if (el.target.value) {
-                setText(`${youTubeURL}${el.target.value}`);
-              } else {
-                setText("");
-              }
-
-              setChanged(true);
-            }}
-          />
-        </div>
-      </label>
-    </>
-  );
+                            setChanged(true);
+                        }}
+                    />
+                </div>
+            </label>
+        </>
+    );
 };
 
 export default YoutubeQR;
