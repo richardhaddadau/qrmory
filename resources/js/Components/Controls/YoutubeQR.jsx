041036b8b8f69c53e198c7f25016a14f8cbd6d1b
richardhaddadau <richard@haddads.net.au>
2023-05-08 18:48:13 +1000

    fix: YouTube control merge

--- resources/js/Components/Controls/YoutubeQR.jsx
+++ resources/js/Components/Controls/YoutubeQR.jsx
@@ -1,55 +1,16 @@
-import { useState } from "react";
-
 const YoutubeQR = ({ setText, setChanged, setNewQR }) => {
-  // States
-  const [urlType, setUrlType] = useState("video");
-  const [youTubeURL, setYouTubeURL] = useState(
-    "https://www.youtube.com/watch?v="
-  );
-
   return (
     <>
       <label className="control-label">
-        Enter YouTube detail:
+        Enter YouTube Link:
         <div className="flex flex-col xl:flex-row flex-nowrap">
-          <select
-            id="protocol-selector"
-            className="mr-2 mt-1 rounded-xl w-44 text-stone-500"
-            onChange={(el) => {
-              const youtubeInput =
-                document.getElementById("youtube-input").value;
-
-              if (el.target.value === "video") {
-                setYouTubeURL("https://www.youtube.com/watch?v=");
-              } else {
-                setYouTubeURL("https://www.youtube.com/");
-              }
-
-              if (youtubeInput) {
-                setText(youTubeURL + youtubeInput);
-              } else {
-                setText("");
-              }
-
-              setNewQR(false);
-              setChanged(true);
-            }}
-          >
-            <option value="video">Video</option>
-            <option value="username">Username</option>
-          </select>
-          <p className="pt-2 text-qrmory-purple-400 font-bold text-lg">
-            {youTubeURL}
-          </p>
           <input
             id="youtube-input"
             type="text"
-            className={
-              "control-input " + (urlType === "video" ? "shortest" : "")
-            }
+            className={"control-input"}
             onChange={(el) => {
               if (el.target.value) {
-                setText(`${youTubeURL}${el.target.value}`);
+                setText(el.target.value);
               } else {
                 setText("");
               }
