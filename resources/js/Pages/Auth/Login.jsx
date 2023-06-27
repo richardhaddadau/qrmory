ba7ae66eb8783904a36ed6db7867e6594818c8c3
richardhaddadau <richard@haddads.net.au>
2023-06-28 04:57:25 +1000

    build: clean out login page

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,16 +1,6 @@
 import { Helmet } from "react-helmet-async";
-import Guest from "../../Layouts/Guest";
-import { Link } from "react-router-dom";
-import Label from "../../Components/Label";
-import { useEffect, useState } from "react";
-import useForm from "../../Helpers/useForm.js";
-import { FaEye, FaEyeSlash } from "react-icons/all";
-import InputError from "../../Components/InputError";
 import NavBar from "../../Components/NavBar.jsx";
-import { FaLock, MdEmail } from "react-icons/all";
 import Standard from "../../Layouts/Standard.jsx";
-import { CheckIfLoggedIn } from "../../Helpers/Firebase/Auth.js";
-import { LogMeIn } from "../../Helpers/Firebase.js";
 
 import React from "react";
 import Userfront from "@userfront/react";
