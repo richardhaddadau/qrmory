e8144b7c077e98304fd451fdf8e2ce4ecf496149
richardhaddadau <richard@haddads.net.au>
2023-07-02 01:16:35 +1000

    build: node server to run database queries from backend

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 // Basic Server
-const express = require("express");
+import express from "express";
+import { createUser } from "./resources/js/Helpers/FaunaDriver.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to COS server");
+  // server react app
 });
 
-app.get("/cors", (req, res) => {
-  res.send("CORS enabled");
+app.get("/api/codes", (req, res) => {
+  const request = req.query;
+  let result;
+
+  createUser(request.user, request.token)
+    .then((data) => {
+      result = data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.send(result);
 });
 
-app.listen(8080, () => {
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
   console.log("Server started on port 8080");
 });
