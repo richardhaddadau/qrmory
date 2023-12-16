feffeb73ddda42d321eb62cdbdccded7c135fd72
Richard Haddad <richard@haddads.net.au>
2023-12-17 08:42:43 +1000

    added clerk configuration

--- qrmory-nextjs/src/app/(public)/layout.js
+++ qrmory-nextjs/src/app/(public)/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import MainFooter from "@/sections/MainFooter";
 
 export const metadata = {
@@ -7,12 +8,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const clerkPK = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
   return (
-    <html lang="en">
-      <body>
-        {children}
-        <MainFooter />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          {children}
+          <MainFooter />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
